6a4fa3ba6d25e4e38cc45764558dabc364cd696b
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-07 19:32:48 -0300

    C34A519 - Using the Runtime API

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
-let message: string;
-
-message = 'Hi there!';
-
-console.log(message);
-
+// let message: string;
+// message = 'Hi there!';
+// console.log(message);
 // deno run app.ts
+
+const text = 'This is a text - and it should be stored in a file';
+
+// Important: Download VS Code Deno extension!!!
+
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
+
+Deno.writeFile('message.txt', data)
+    .then(result => {
+        console.log('Wrote to file successfully');
+    })
+    .catch(err => {
+        console.log(err);
+    });

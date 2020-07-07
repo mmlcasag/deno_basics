29b16b410169957222ed3b75fabd953d0cd450a8
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-07 19:40:55 -0300

    C34A520 - Working with Deno Permissions

--- app.ts
+++ app.ts
@@ -12,8 +12,17 @@ const data = encoder.encode(text);
 
 Deno.writeFile('message.txt', data)
     .then(result => {
-        console.log('Wrote to file successfully');
+        console.log('Wrote successfully to the file');
     })
     .catch(err => {
         console.log(err);
     });
+
+// if you try to run this code using "deno run app.ts" you will get a permission error
+// this happens because node is secure by default
+// this means you need permission to read or write files, for example
+// and how to give permission to write files?
+// "deno run --allow-write app.ts" --> this gives permission to write any file
+// "deno run --allow-read app.ts --> this gives permission to read any file
+// "deno run --allow-write=message.txt app.ts --> this gives permission to write just message.txt file
+// so, now i will run "deno run --allow-write=message.txt app.ts" and it will work!

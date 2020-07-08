af7a94b92998f5e492ab3caf44febe42fc5980bc
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-08 08:32:39 -0300

    C35A533 - Handling CORS Errors

--- deno/app.ts
+++ deno/app.ts
@@ -3,6 +3,14 @@ import todoRoutes from "./routes.ts";
 
 const app = new Application();
 
+// handling CORS errors
+app.use(async (ctx, next) => {
+    ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+    ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    await next();
+});
+
 app.use(todoRoutes.routes());
 app.use(todoRoutes.allowedMethods());
 
